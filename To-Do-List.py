1fd403722eba186b32ee1c2184cc92fa30575250
CML <lusiolac@bzz.ch>
2025-01-21 17:52:34 +0100

    Finito

--- To-Do-List.py
+++ To-Do-List.py
@@ -5,3 +5,17 @@ def display_tasks(tasks):
         print("Aktuelle Aufgaben:")
         for index, task in enumerate(tasks, start=1):
             print(f"{index}. {task}")
+
+def main():
+    tasks = []
+
+    while True:
+        print("\nAufgabenverwaltung")
+        print("1. Eine Aufgabe hinzufügen")
+        print("2. Alle Aufgaben anzeigen")
+        print("3. Aufgabe löschen")
+        print("4. Programm beenden")
+
+        choice = input("Bitte eine Option auswählen (1-4): ")
+if __name__ == "__main__":
+    main()
